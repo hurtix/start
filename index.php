8497205808e4c6ac165240c816d42a75dced97a7
hurtix <hurtix@gmail.com>
2022-10-19 07:34:52 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
-<?php echo "hola mundo, its me" ?>
+<<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Hello Heroku</title>
+</head>
+<body>
+<h1><?php echo "hola mundo, its me" ?></h1>
+</body>
+</html>
 
+
+ 
